08e779f8a1b68a99b9302255ac083dd97c70f247
Suyash Verma <suyashvermaabes27@gmail.com>
2025-09-17 10:01:41 +0530

    Add files via upload

---
+++ Lecture1/practice/test.js
@@ -0,0 +1,5 @@
+let parent=document.querySelector(".secondEelemnt");
+console.log(parent.nextElementSibling);
+console.log(parent.previousElementSibling);
+let test=document.querySelectorAll("#data");
+console.log(test);
