950ac802bb3b4035244696fef61d48a461dac30f
Suyash Verma <suyashvermaabes27@gmail.com>
2025-11-08 21:07:30 +0530

    Add files via upload

---
+++ JS revision/Lecture1/script1.js
@@ -0,0 +1,4 @@
+// clear(); is used to clear the browser console.
+console.log("Hello World!");
+// All the outputs are viewed in consol window in webpage 
+// console.log() is not needed to print in consol window 
