7d8a82f801dd129f89ca3cb847e7bd61aa3da5ed
Suyash Verma <suyashvermaabes27@gmail.com>
2025-11-09 15:56:06 +0530

    Add files via upload

---
+++ JS revision/Lecture2/script2.js
@@ -0,0 +1,6 @@
+//Typeof is used to get the which type of datatype it is.
+//`` if we use backticks then we can write in new line
+//with ""or'' we cannot write in the new line
+//Press shift+enter to get newline in console
+//if we write + or - before string then it get converted into number in console.
+//if we write parseInt before string then it convert string into number before any character will get print ex 12vfsba0 then 12 will print.
