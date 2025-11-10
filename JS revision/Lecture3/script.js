826be8b8e506d76941887d01462301dda1502e90
Suyash Verma <suyashvermaabes27@gmail.com>
2025-11-10 10:57:18 +0530

    Add files via upload

---
+++ JS revision/Lecture3/script.js
@@ -0,0 +1,4 @@
+//Let variable is used to create the variable whose vlue can be changed in furute or decalered later.
+//const cariable is used to create the variable whose value cannot be changed in future and need to initialized when decalerd.
+//var can be accsed outside the block whereas let and const cannot be accsed outside the block.
+//if we accses var before initialization then it will give undefined whereas let and const will give error.
